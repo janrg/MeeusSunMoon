0896e557bf793a9317574818a26de83c6787dc51
janrg <janrg@users.noreply.github.com>
2020-04-11 13:55:46 +0200

    Update dependencies

--- dist/meeussunmoon.js
+++ dist/meeussunmoon.js
@@ -11,6 +11,8 @@
 }(this, (function (exports) { 'use strict';
 
   function _typeof(obj) {
+    "@babel/helpers - typeof";
+
     if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") {
       _typeof = function (obj) {
         return typeof obj;
