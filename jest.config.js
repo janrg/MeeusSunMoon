cb33db23e9c48b91abd6a0830c365b2746f0019d
Brett Zamir <brettz9@yahoo.com>
2020-08-17 00:38:15 +0800

    - Testing: Enforce coverage thresholds to 100%

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
     collectCoverage: true,
     coverageDirectory: 'coverage',
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100,
+        },
+    },
     preset: 'ts-jest',
     setupFilesAfterEnv: ['jest-sorted'],
     testEnvironment: 'node',
