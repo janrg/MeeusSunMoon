501fa23f2003fa4d9950999427f6e5d690c41174
janrg <janrg@users.noreply.github.com>
2020-12-18 19:48:30 +0100

    Update dependencies

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,8 +1,19 @@
 import { DateTime as LuxonDateTime } from 'luxon';
 
+export type MoonPhaseNumber = 0 | 1 | 2 | 3;
+
+export type RiseSetFlag = 'RISE' | 'SET';
+
+export type NoEventCode = 'SUN_HIGH' | 'SUN_LOW';
+
 // eslint-disable-next-line require-jsdoc
 export class DateTime extends LuxonDateTime {
     errorCode?: string;
+}
+
+export interface DateFormatKeys {
+    SUN_HIGH: string;
+    SUN_LOW: string;
 }
 
 export interface MeeusSunMoonSettings {
@@ -11,18 +22,7 @@ export interface MeeusSunMoonSettings {
     dateFormatKeys?: DateFormatKeys;
 }
 
-export interface DateFormatKeys {
-    SUN_HIGH: string;
-    SUN_LOW: string;
-}
-
 export interface MoonPhase {
     datetime: LuxonDateTime,
     phase: MoonPhaseNumber,
 }
-
-export type MoonPhaseNumber = 0 | 1 | 2 | 3;
-
-export type RiseSetFlag = 'RISE' | 'SET';
-
-export type NoEventCode = 'SUN_HIGH' | 'SUN_LOW';
